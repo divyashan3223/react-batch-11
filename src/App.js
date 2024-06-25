467d9e2a4dc9ca8d48061584b9c026817d204656
frenzo technology <divyashan3223@gmail.com>
2024-06-25 13:02:58 +0530

    props introo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Button from "./components/button/classComponent/button/Button";
 function App() {
   return (
     <div className="App">
+      <Button content={"Click"} bg={"red"} />
+      <Button content={"download"} bg={"yellow"} />
+      <Button content={"cancel"} color={"white"} />
       <Button />
     </div>
   );
--- src/components/button/classComponent/button/Button.jsx
+++ src/components/button/classComponent/button/Button.jsx
@@ -1,31 +1,56 @@
 import "./button.css";
 export const Heading = () => {
-  return <h1>heading tag</h1>;
+  return <h1>props in react</h1>;
 };
 
-//? inline another style
+// TODO  using props key
 
-// const Button = () => {
-//     const style={ backgroundColor: "blue", color: "white" }
+//props
+// const Button = (props) => {
 //   return (
-//     <div>
-//       <Heading />
-//       <button style={}>
-//         submit
-//       </button>
+//     <div className="container">
+//       <button>{props.content}</button>
 //     </div>
 //   );
 // };
 
 // export default Button;
 
-//! .external css
-const Button = () => {
-  const style = { backgroundColor: "blue", color: "white" };
+// * props getting direct argument name
+
+//props
+// const Button = ({content}) => {
+//   return (
+//     <div className="container">
+//       <button>{content}</button>
+//     </div>
+//   );
+// };
+
+// export default Button;
+
+// ? background values applied directly and default props
+
+// const Button = ({ content = "submit", bg, color }) => {
+//   return (
+//     <div className="container">
+//       <button style={{ backgroundColor: `${bg}`,color:`${color}` }}>{content}</button>
+//     </div>
+//   );
+// };
+
+// export default Button;
+
+// !background values stored a variable
+
+const Button = ({ content = "submit", bg, color }) => {
+  const data = {
+    backgroundColor: bg,
+    color: color,
+  };
   return (
     <div className="container">
-      <Heading />
-      <button>submit</button>
+      <button style={data}>{content}</button>
     </div>
   );
 };
