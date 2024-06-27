23eacd22b75849be0f062d0a06f29e46b822069b
frenzo technology <divyashan3223@gmail.com>
2024-06-27 12:27:39 +0530

    closure function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import Button from "./components/button/classComponent/button/Button";
 
 function App() {
+  const handleClick = () => {
+    alert("data was cancled");
+  };
+  const data = () => {
+    alert("data was submitted+b");
+  };
   return (
     <div className="App">
-      <Button content={"Click"} bg={"red"} />
-      <Button content={"download"} bg={"yellow"} />
-      <Button content={"cancel"} color={"white"} />
-      <Button />
+      <Button Click={handleClick} content={"cancel"} />
+      <Button Click={data} content={"submited"} />
     </div>
   );
 }
--- src/components/button/classComponent/button/Button.jsx
+++ src/components/button/classComponent/button/Button.jsx
@@ -1,56 +1,8 @@
 import "./button.css";
-export const Heading = () => {
-  return <h1>props in react</h1>;
-};
-
-// TODO  using props key
-
-//props
-// const Button = (props) => {
-//   return (
-//     <div className="container">
-//       <button>{props.content}</button>
-//     </div>
-//   );
-// };
-
-// export default Button;
-
-// * props getting direct argument name
-
-//props
-// const Button = ({content}) => {
-//   return (
-//     <div className="container">
-//       <button>{content}</button>
-//     </div>
-//   );
-// };
-
-// export default Button;
-
-// ? background values applied directly and default props
-
-// const Button = ({ content = "submit", bg, color }) => {
-//   return (
-//     <div className="container">
-//       <button style={{ backgroundColor: `${bg}`,color:`${color}` }}>{content}</button>
-//     </div>
-//   );
-// };
-
-// export default Button;
-
-// !background values stored a variable
-
-const Button = ({ content = "submit", bg, color }) => {
-  const data = {
-    backgroundColor: bg,
-    color: color,
-  };
+const Button = ({ Click, content }) => {
   return (
     <div className="container">
-      <button style={data}>{content}</button>
+      <button onClick={Click}>{content}</button>
     </div>
   );
 };
