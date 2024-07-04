d90ab7c959a4816538bed680e792cf8e9257eef1
frenzo technology <divyashan3223@gmail.com>
2024-07-04 12:27:06 +0530

    useRef

--- src/components/button/classComponent/button/Button.jsx
+++ src/components/button/classComponent/button/Button.jsx
@@ -1,38 +1,14 @@
-import "./button.css";
-import { useEffect, useState } from "react";
-const Button = () => {
-  const [data, setData] = useState(0);
-  const [data1, setData1] = useState(0);
-  useEffect(() => {   
-    console.log("data is working in backend");
-  }, [data1]);
-  let handleClick = () => {
-    setData((data) => data + 1);
+import { useRef } from "react";
 
-    console.log(data);
-  };
-  let handleClick1 = () => {
-    setData1((data1) => data1 + 1);
-  };
-  let decrement = () => {
-    setData((data) => data - 1);
+function Button() {
+  let ref = useRef(0);
 
-    console.log(data);
-  };
-  let decrement1 = () => {
-    setData1((data1) => data1 - 1);
-  };
+  function handleClick() {
+    ref.current = ref.current + 1;
+    alert("You clicked " + ref.current + " times!");
+  }
 
-  return (
-    <div className="container">
-      <h1>count:{data}</h1>
-      <h1>count1:{data1}</h1>
-      <button onClick={handleClick}>increment</button>
-      <button onClick={decrement}>decrement</button>
-      <button onClick={handleClick1}>increment</button>
-      <button onClick={decrement1}>decrement</button>
-    </div>
-  );
-};
+  return <button onClick={handleClick}>Click me!</button>;
+}
 
 export default Button;
