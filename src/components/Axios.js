df29cdef36a574899d10bf3f29b5a96f1f2f7d97
frenzo technology <divyashan3223@gmail.com>
2024-07-25 15:21:52 +0530

    axios

--- src/components/Axios.js
+++ src/components/Axios.js
@@ -9,28 +9,30 @@ const CrudOperations = () => {
 
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/users")
-      .then((response) => setItems(response.data.slice(0, 10)))
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => setItems(response.data.slice(0, 2)))
       .catch((error) => setError(error));
   }, []);
 
   // Create new item
   const createItem = () => {
     axios
-      .post("https://jsonplaceholder.typicode.com/users", {
+      .post("https://jsonplaceholder.typicode.com/posts", {
         title: item,
         body: item,
+        userId: 1,
       })
-      .then((response) => setItems([...items, response.data]))
+      .then((response) => setItems([response.data, ...items]))
       .catch((error) => setError(error));
   };
 
   // Update an existing item
   const updateItem = (id) => {
     axios
-      .put(`https://jsonplaceholder.typicode.com/users/${id}`, {
+      .put(`https://jsonplaceholder.typicode.com/posts/${id}`, {
         title: item,
         body: item,
+        userId: 1,
       })
       .then((response) => {
         setItems(items.map((i) => (i.id === id ? response.data : i)));
@@ -42,7 +44,7 @@ const CrudOperations = () => {
   // Delete an item
   const deleteItem = (id) => {
     axios
-      .delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then(() => setItems(items.filter((i) => i.id !== id)))
       .catch((error) => setError(error));
   };
